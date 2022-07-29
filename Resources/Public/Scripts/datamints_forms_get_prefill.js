4a39935bf01559530cd969b19e1c1dd0138bec4d
Mark Weisgerber <m.weisgerber@datamints.com>
2022-07-29 13:49:03 +0200

    R-33505 powermail support

--- Resources/Public/Scripts/datamints_forms_get_prefill.js
+++ Resources/Public/Scripts/datamints_forms_get_prefill.js
@@ -3,11 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
     // scans all forms, unfortunately theres no class or something like that to identify a EXT:forms related form...
     forms.forEach(function (currentForm, index) {
         const formId = currentForm.id;
-        if(formId != null && typeof formId === 'string'){
+        const isPowermail = currentForm.classList.contains('powermail_form');
+        if((formId != null && typeof formId === 'string') || isPowermail){
             // get current url params and check if it begins with "form_" and additionally if there is a dom-element with the given id existing
             new URL(window.location).searchParams.forEach(function (paramValue, paramKey) {
                 if(paramKey.startsWith('form_')){
-                    let input = currentForm.querySelector('#'+formId+'-'+paramKey.replace('form_',''));
+                    let querySelector = '#'+formId+'-'+paramKey.replace('form_','');
+                    if(isPowermail){
+                        querySelector = '#powermail_field_'+paramKey.replace('form_','');
+                    }
+                    let input = currentForm.querySelector(querySelector);
                     if(input != null){
                         input.value = paramValue;
                     }
